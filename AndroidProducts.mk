552025660474ad3e93531411728993881881aac7
DY3413 <milosix3413@gmail.com>
2024-02-05 08:14:53 +0900

    gta4lwifi: Adapt for PixelOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gta4lwifi.mk
+    $(LOCAL_DIR)/aosp_gta4lwifi.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_gta4lwifi-user \
-    lineage_gta4lwifi-userdebug \
-    lineage_gta4lwifi-eng
+    aosp_gta4lwifi-user \
+    aosp_gta4lwifi-userdebug \
+    aosp_gta4lwifi-eng
--- aosp_gta4lwifi.mk
+++ aosp_gta4lwifi.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Inherit from gta4lwifi device
 $(call inherit-product, device/samsung/gta4lwifi/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_tablet.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta4lwifi
-PRODUCT_NAME := lineage_gta4lwifi
+PRODUCT_NAME := aosp_gta4lwifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T500
 PRODUCT_MANUFACTURER := samsung
